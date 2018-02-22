2c100fefc48caf055dff93e5142e4cb20e6e8149
Roger Camps <rogercamps@me.com>
2018-02-21 16:13:14 -0800

    install gulp and work on css architecture

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import')
+
+
+gulp.task('default', function() {
+  console.log('hooray!!!!!!!');
+    
+})
+
+gulp.task('html', function() {
+  return gulp.src('./app/assets/styles/styles.css')
+  .pipe(postcss([cssImport, nested, cssvars, autoprefixer]))
+  .pipe(gulp.dest('./app/temp/styles'))
+})
+
+gulp.task('watch', function() {
+  watch('./app/index.html', function() {
+    gulp.start('html')
+  })
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('html')
+  })
+
+  .on('error', function(errorInfo) {
+    console.log(errorInfo.toString())
+    this.emit('end')
+  })
+})
